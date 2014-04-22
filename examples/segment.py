32aafe0ca452c05fc7cbe4ff996961d625209ece
Serrano Pereira <serrano.pereira@gmail.com>
2014-04-22 11:48:53 +0200

    Added feature for toggling between original and segmented image

--- examples/segment.py
+++ examples/segment.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""This program is a demonstration of image segmentation.
+
+The following key bindings are available:
+* O - see the original image
+* S - see the segmented image
+* Q - exit
+"""
+
 import argparse
 import os
 import sys
@@ -14,10 +22,12 @@ import numpy as np
 import features as ft
 
 def main():
+    print __doc__
+
     parser = argparse.ArgumentParser(description='Test image segmentation')
     parser.add_argument('image', metavar='FILE', help='Input image')
-    parser.add_argument('-i', '--iters', metavar='N', type=int, default=5, help="The number of grabCut iterations. Default is 5.")
-    parser.add_argument('-m', '--margin', metavar='N', type=int, default=1, help="The margin of the foreground rectangle from the edges. Default is 1.")
+    parser.add_argument('--iters', metavar='N', type=int, default=5, help="The number of grabCut iterations. Default is 5.")
+    parser.add_argument('--margin', metavar='N', type=int, default=1, help="The margin of the foreground rectangle from the edges. Default is 1.")
     parser.add_argument('--maxdim', metavar='N', type=float, default=None, help="Limit the maximum dimension for an input image. The input image is resized if width or height is larger than N. Default is no limit.")
     parser.add_argument('--mindim', metavar='N', type=int, default=100, help="Limit the minimum dimension for input and output images. Images with a smaller width or height are ignored. Default is 100.")
     args = parser.parse_args()
@@ -47,7 +57,17 @@ def main():
     # Display the image in a window.
     cv2.namedWindow('image')
     cv2.imshow('image', img_masked)
-    cv2.waitKey(0)
+
+    while True:
+        k = cv2.waitKey(0) & 0xFF
+
+        if k == ord('o'):
+            cv2.imshow('image', img)
+        elif k == ord('s'):
+            cv2.imshow('image', img_masked)
+        elif k == ord('q'):
+            break
+
     cv2.destroyAllWindows()
 
     return 0
