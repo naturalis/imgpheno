eca6c5a8adc97bab6ee298bd18950531ca052eea
Serrano Pereira <serrano.pereira@gmail.com>
2014-04-23 11:55:03 +0200

    Added a unit test for shape:360

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 test:
-	nosetests
+	python -m unittest discover -v
--- tests/test_features.py
+++ tests/test_features.py
@@ -10,6 +10,7 @@ import cv2
 from context import features as ft
 
 IMAGE_SLIPPER = "data/slipper.jpg"
+IMAGES_ERYCINA = ("data/erycina.jpg", "data/erycina2.jpg")
 MAXDIM = 500
 
 class TestFeatures(unittest.TestCase):
@@ -54,6 +55,84 @@ class TestFeatures(unittest.TestCase):
 
         # Two flowers in image should produce two segments.
         self.assertEqual( len(list(segments)), 2 )
+
+    def test_shape_360(self):
+        """Test the shape:360 feature.
+
+        Extracts the shape from two slightly rotated versions of the same
+        image. Then the medium square error between the two extracted shapes is
+        calculated and checked.
+        """
+        shape = []
+        for i, path in enumerate(IMAGES_ERYCINA):
+            im_path = os.path.join(self.base_dir, path)
+            img = cv2.imread(im_path)
+            if img == None or img.size == 0:
+                raise SystemError("Failed to read %s" % im_path)
+
+            # Resize the image if it is larger then the threshold.
+            max_px = max(img.shape[:2])
+            if max_px > MAXDIM:
+                rf = float(MAXDIM) / max_px
+                img = cv2.resize(img, None, fx=rf, fy=rf)
+
+            # Perform segmentation.
+            mask = ft.segment(img, 5, 1)
+
+            # Create a binary mask. Foreground is made white, background black.
+            bin_mask = np.where((mask==cv2.GC_FGD) + (mask==cv2.GC_PR_FGD), 255, 0).astype('uint8')
+
+            # Obtain contours (all points) from the mask.
+            contour = ft.get_largest_countour(bin_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+            # Fit an ellipse on the contour to get the rotation angle.
+            box = cv2.fitEllipse(contour)
+            rotation = int(box[2])
+
+            # Extract the shape.
+            intersects, center = ft.shape_360(contour, rotation)
+
+            # For each angle save the mean distance from center to contour and
+            # the standard deviation for the distances.
+            means = []
+            sds = []
+            for angle in range(360):
+                distances = []
+                for p in intersects[angle]:
+                    d = ft.point_dist(center, p)
+                    distances.append(d)
+
+                if len(distances) == 0:
+                    mean = 0
+                    sd = 0
+                else:
+                    mean = np.mean(distances, dtype=np.float32)
+                    if len(distances) > 1:
+                        sd = np.std(distances, ddof=1, dtype=np.float32)
+                    else:
+                        sd = 0
+
+                means.append(mean)
+                sds.append(sd)
+
+            # Normalize and save result.
+            means = cv2.normalize(np.array(means), None, -1, 1, cv2.NORM_MINMAX)
+            sds = cv2.normalize(np.array(sds), None, -1, 1, cv2.NORM_MINMAX)
+            shape.append([means, sds])
+
+        # Check the medium square error for the means.
+        diff_means = 0
+        for i in range(360):
+            diff_means += abs(shape[0][0][i] - shape[1][0][i])
+        mse = (diff_means / 360) ** 2
+        self.assertLess(mse, 0.05)
+
+        # Check the medium square error for the standard deviations.
+        diff_sds = 0
+        for i in range(360):
+            diff_sds += abs(shape[0][1][i] - shape[1][1][i])
+        mse = (diff_sds / 360) ** 2
+        self.assertLess(mse, 0.05)
 
 if __name__ == '__main__':
     unittest.main()
